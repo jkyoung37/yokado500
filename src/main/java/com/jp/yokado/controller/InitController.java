b138378bb098641bafa5491042e75da958e76f3d
Apple-J <jkyoung37@gmail.com>
2020-12-20 17:32:32 +0900

    stream適用

--- src/main/java/com/jp/yokado/controller/InitController.java
+++ src/main/java/com/jp/yokado/controller/InitController.java
@@ -2,6 +2,7 @@ package com.jp.yokado.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.NumberUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.jp.yokado.form.ReceiptForm;
 import com.jp.yokado.model.Receipt;
@@ -21,114 +24,113 @@ import com.jp.yokado.validation.Validations;
 @RequestMapping("/init")
 public class InitController {
 
-    @ModelAttribute
-    public ReceiptForm setupForm() {
-        return new ReceiptForm();
-    }
+	@ModelAttribute
+	public ReceiptForm setupForm() {
+		return new ReceiptForm();
+	}
 
-    @GetMapping
-    public String init(ReceiptForm form) {
-        form.appendRow();
-        return "index";
-    }
+	@GetMapping
+	public String init(ReceiptForm form) {
+		form.appendRow();
+		return "index";
+	}
+	
+	@PostMapping(params = "init")
+	public String moveInit(ReceiptForm form, BindingResult result) {
+		form.appendRow();
+		return "index";
+	}
 
-    @PostMapping(params = "appendRow")
-    public String appendRow(ReceiptForm form, BindingResult result) {
-        form.appendRow();
-        return "index";
-    }
+	@PostMapping(params = "appendRow")
+	public String appendRow(ReceiptForm form, BindingResult result) {
+		form.appendRow();
+		return "index";
+	}
 
-    @PostMapping(params = "removeIndex")
-    public String submit(ReceiptForm form, @RequestParam int removeIndex, BindingResult result) {
-        form.removeRow(removeIndex);
-        return "index";
-    }
+	@PostMapping(params = "removeIndex")
+	public String submit(ReceiptForm form, @RequestParam int removeIndex, BindingResult result) {
+		form.removeRow(removeIndex);
+		return "index";
+	}
 
-    @PostMapping(params = "doCalculate")
-    public String submit(Model model, ReceiptForm form, BindingResult result) {
+	@PostMapping(params = "doCalculate")
+	public String submit(Model model, ReceiptForm form, BindingResult result) {
 
-        if(result.hasErrors()){
-            return "index";
-        }
-       
-        if(hasErrors(form)){
-            return "index";
-        }
+		if (result.hasErrors()) {
+			return "index";
+		}
 
-        List<int[]> allPoints = new ArrayList<>();
-        boolean isOverMaxpoint = Utils.OverMaxPoint(form.getReceipts());
-        Integer minSum = 0;
-        List<int[]> minSumResult = new ArrayList<>();
+		if (this.hasErrors(form)) {
+			return "index";
+		}
 
-        if (isOverMaxpoint) {
-            allPoints = getAllPoint(form);
+		List<ArrayList<Integer>> allPoints = new ArrayList<>();
+		List<ArrayList<Integer>> minSumResult = new ArrayList<>();
 
-            // Maxpoints Pick up
-            for (int i = 0; i < allPoints.size(); i++) {
-                int[] items = allPoints.get(i);
-                int sum = 0;
+		
+		Integer minSum = 0;
 
-                for (int j = 0; j < items.length; j++) {
-                    sum += items[j];
-                }
+		boolean isOverMaxpoint = Utils.OverMaxPoint(form.getReceipts());
+		
+		if (isOverMaxpoint) {
+			allPoints = this.getAllPoint(form);
+			for (ArrayList<Integer> items : allPoints) {
+				int sum = items.stream().mapToInt(Integer::intValue).sum();
+				if (sum >= Numbers.MAX_POINT) {
+					if (sum <= minSum.intValue() || minSum.intValue() == 0) {
+						minSum = sum;
+						minSumResult.add(items);
+					}
+				}
+			}
+			model.addAttribute("results",this.getFinalResult(minSumResult));
+			return "resultPage";
+		
+		} else {
+			return "index";
+		}
+		
 
-                if (sum >= Numbers.MAX_POINT) {
-                    if (sum <= minSum.intValue() || minSum.intValue() == 0) {
-                        minSum = sum;
-                        minSumResult.add(items);
-                    }
-                }
-            }
-        } else {
-            return "index";
-        }
+	}
+	
+	public List<String> getFinalResult(List<ArrayList<Integer>> items){
+		
+		List<String> result = new ArrayList<>();
+		
+		for(ArrayList<Integer> item : items) {
+			String joinedString = item.stream().map(x -> x.toString()).collect(Collectors.joining("・","・"," "));
+			result.add(joinedString);
+		}
+		return result;
+	}
+	
+	private List<ArrayList<Integer>> getAllPoint(ReceiptForm form) {
+		List<ArrayList<Integer>> result = new ArrayList<>();
+		List<Receipt> itemLists = form.getReceipts();
+		int[] points = Utils.convertIntegers(itemLists);
+		int n = points.length;
+		boolean[] visited = new boolean[n];
 
-        if(minSumResult.size() == 0){
-            return "index";
-        }else{
+		Combination.clear();
+		for (int i = 1; i <= n; i++) {
+			Combination.combination(points, visited, 0, n, i);
+		}
 
-            int[] miniNb;
-            List<StringBuilder> resultList = new ArrayList();
-            for(int[] item : minSumResult){
-                miniNb = item;
-                StringBuilder resultMSG = new StringBuilder();
-                for (int i = 0; i < miniNb.length ; i++) {
-                    int inputNm = miniNb[i];
-                    if(i==0){
-                        resultMSG.append(i+1);
-                        resultMSG.append(" : ");
-                    }
-                    resultMSG.append(inputNm);
-                }
-                resultList.add(resultMSG);
-            }
+		List<int[]> tempLists = Combination.getItem();
 
-            model.addAttribute("result", resultList);
-            return "resultPage";
-        }
-    }
+		for (int[] items : tempLists) {
+			List<Integer> tempList = Arrays.stream(items).boxed().collect(Collectors.toList());
+			result.add((ArrayList<Integer>) tempList);
+		}
 
-    private List<int[]> getAllPoint(ReceiptForm form) {
-        List<Receipt> itemLists = form.getReceipts();
-        int[] points = Utils.convertIntegers(itemLists);
-        int n = points.length;
-        boolean[] visited = new boolean[n];
+		return result;
+	}
 
-        Combination.clear();
-        for (int i = 1; i <= n; i++) {
-            Combination.combination(points, visited, 0, n, i);
-        }
-        return Combination.getItem();
-    }
-
-    private boolean hasErrors(ReceiptForm form){
-
-        boolean isNull = Validations.isNull(form.getReceipts());
-        if(isNull){
-            return true;
-        }
-        return false;
-    
-    }
-
+	private boolean hasErrors(ReceiptForm form) {
+		boolean isNull = Validations.isNull(form.getReceipts());
+		if (isNull) {
+			return true;
+		}
+		return false;
+	}
 }
--- src/main/java/com/jp/yokado/utils/Utils.java
+++ src/main/java/com/jp/yokado/utils/Utils.java
@@ -6,27 +6,28 @@ import com.jp.yokado.model.Receipt;
 
 public class Utils {
 
-    public static boolean OverMaxPoint(List<Receipt> receipts) {
-        double allSumValue = receipts.stream().mapToInt(Receipt::getPoint).sum();
-        if (allSumValue < Numbers.MAX_POINT) {
-            return false;
-        }
-        return true;
-    }
+	public static boolean OverMaxPoint(List<Receipt> receipts) {
+		double allSumValue = receipts.stream().mapToInt(Receipt::getPoint).sum();
+		if (allSumValue < Numbers.MAX_POINT) {
+			return false;
+		}
+		return true;
+	}
 
-    public static int[] convertIntegers(List<Receipt> integers) {
-        int[] ret = new int[integers.size()];
-        for (int i = 0; i < ret.length; i++) {
-            ret[i] = integers.get(i).getPoint();
-        }
-        return ret;
-    }
-    
-    public static int[] convertIntegers2(List<Integer> integers) {
-        int[] ret = new int[integers.size()];
-        for (int i = 0; i < ret.length; i++) {
-            ret[i] = integers.get(i).intValue();
-        }
-        return ret;
-    }
+	public static int[] convertIntegers(List<Receipt> integers) {
+		int[] ret = new int[integers.size()];
+		for (int i = 0; i < ret.length; i++) {
+			ret[i] = integers.get(i).getPoint();
+		}
+		return ret;
+	}
+
+	public static int[] convertIntegers2(List<Integer> integers) {
+		int[] ret = new int[integers.size()];
+		for (int i = 0; i < ret.length; i++) {
+			ret[i] = integers.get(i).intValue();
+		}
+		return ret;
+	}
+	
 }
