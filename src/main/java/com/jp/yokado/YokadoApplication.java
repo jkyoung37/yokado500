4d514287baedf3b9c2fef462c8e90807af273ac2
DESKTOP-5QB9KIT\jks55 <jkyoung37@gmail.com>
2021-03-18 19:53:44 +0900

    【成功】  WebMvcConfigurerに変更し build OK

--- src/main/java/com/jp/yokado/YokadoApplication.java
+++ src/main/java/com/jp/yokado/YokadoApplication.java
@@ -1,12 +1,24 @@
 package com.jp.yokado;
 
+import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import com.jp.yokado.resolver.UserArgumentResolver;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @SpringBootApplication
-public class YokadoApplication {
+public class YokadoApplication implements WebMvcConfigurer {
+  private final UserArgumentResolver userArgumentResolver;
 
-	public static void main(String[] args) {
-		SpringApplication.run(YokadoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(YokadoApplication.class, args);
+  }
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+    argumentResolvers.add(userArgumentResolver);
+  }
 }
